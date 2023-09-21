082b3e0c3e886a4c1c2fa22cf7caaa9da5087be9
Amol Jaiswal <114354608+amolj2003@users.noreply.github.com>
2023-09-21 20:57:11 +0530

    Update MediumCard.tsx

--- components/MediumCard.tsx
+++ components/MediumCard.tsx
@@ -6,7 +6,7 @@ function MediumCard({img,title}) {
     <div className="cursor-pointer hover:scale-105 transform transition duration-300 ease-out">
         <div className="relative h-80 w-80">
         <h1> hello </h1>
-        <Image src={img} layout="fill"  className="rounded-xl"/>
+        <Image src={img} layout="fill"  alt="Picture of the author" className="rounded-xl "/>
         </div>
         <h3 className="text-2xl mt-3">{title}</h3>  
     </div> 
